fe920fe2c17228c483317d36c05833d55fb7f0ee
Ayush Dwivedi <21cencturyayush@gmail.com>
2020-08-02 11:45:31 +0530

    added warning to buildtest.sh

--- test/buildtest.sh
+++ test/buildtest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#do not run this script until buffer and lrbuffer are completed
+
 INCLUDE=../include/
 SRC=../src/
-gcc -g -o bin/$1 $SRC*.c $1.c -I$INCLUDE
+#gcc -g -o bin/$1 $SRC*.c $1.c -I$INCLUDE
