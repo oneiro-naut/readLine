875319bdcbc6866112aa8d60892e28ab5a749e8d
Ayush Dwivedi <21cencturyayush@gmail.com>
2020-08-02 11:00:09 +0530

    editted buildbuffertest.sh file added comments
    There is an issue with .gitignore file

--- test/buildbuffertest.sh
+++ test/buildbuffertest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# create a bin dir inside your test dir before running this script
 INCLUDE=../include/
 SRC=../src/
 CC=gcc
-
+#has issue since gitignore ingores the bin directory completely
+#might show bin no such file or directory
 $CC -g -o bin/buffertest buffertest.c $SRC/buffer.c -I$INCLUDE 
